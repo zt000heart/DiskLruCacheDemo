f9917a32acaf87a2c239f23d762f01bc644195b2
zhangtao21 <zhangtao21@meituan.com>
2015-12-21 18:28:56 +0800

    Demo

---
+++ app/src/androidTest/java/com/meeting/zhangtao21/disklrucachedemo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.meeting.zhangtao21.disklrucachedemo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
